2741f4ba53803d5828184a7a85ee64bca4c437bc
cami1330 <ycojeda@uts.edu.co>
2026-04-11 17:32:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,9 +21,24 @@ function mostrarProductos(){
   lista.innerHTML="";
   inventario.forEach((producto,index)=>{
     let li =document.createElement("li");
-    li.innerHTML=`${producto.nombre} -$${producto.precio}
-     <button  onclick="eliminarProducto(${index})">Eliminar</button> 
-      <button onclick="editarProducto(${index})">Editar</button>`;
+    li.innerHTML=`<span>
+  <strong>${producto.nombre}</strong><br>
+  $${producto.precio} (${producto.categoria})
+</span>
+     <button  onclick="eliminarProducto(${index})">❌</button> 
+      <button onclick="mostrarFormularioEditar(${index})">✏️</button>
+      
+      <div id="editar-${index}" style="display:none; margin-top:10px;">
+      <input type="text"id="nombre-${index}" value="${producto.nombre}">
+      <input type="number" id="precio-${index}" value="${producto.precio}">
+      <input type="text" id="categoria-${index}" value="${producto.categoria}">
+        <button onclick="guardarEdicion(${index})">Guardar</button>
+         <button onclick="cancelarGuardarDatos(${index})">Cancelar</button>
+      
+      
+      </div>
+      
+      `;
     lista.appendChild(li);
   })
 }
@@ -37,6 +52,7 @@ function agregarProducto(){
   
   let nombre=document.getElementById("nombre").value;
   let precio=document.getElementById("precio").value;
+  let categoria=document.getElementById("categoria").value;
   
   if(nombre===""||precio===""){
     alert("por favor completa todos los campos");
@@ -45,7 +61,8 @@ function agregarProducto(){
   
   inventario.push({
     nombre:nombre,
-  precio:Number(precio)
+  precio:Number(precio),
+    categoria:categoria
   });
   
 
@@ -74,17 +91,19 @@ function eliminarProducto(index){
    mostrarMensaje("Producto eliminado");
 }
 
-function editarProducto(index){
-  let nuevoNombre= prompt("Nuevo nombre:");
-  let nuevoPrecio = prompt("Nuevo precio:");
+function guardarEdicion(index) {
+ 
+  let nuevoNombre=document.getElementById(`nombre-${index}`).value;
+  let nuevoPrecio = document.getElementById(`precio-${index}`).value;
+  let nuevaCategoria = document.getElementById(`categoria-${index}`).value;
+
+  inventario[index].nombre = nuevoNombre;
+  inventario[index].precio = Number(nuevoPrecio);
+  inventario[index].categoria = nuevaCategoria;
+
   
-  if (nuevoNombre==="" || nuevoPrecio===""){
-    alert("datos invalidos");
-    return
-  };
-  
-  inventario[index].nombre=nuevoNombre;
-  inventario[index].precio=nuevoPrecio;
+  let form=document.getElementById(`editar-${index}`);
+  form.style.display="none";
   guardarDatos();
   mostrarProductos();
   mostrarMensaje("Producto actualizado");
@@ -117,11 +136,41 @@ function ordenarMayor() {
 }
 
 function mostrarMensaje(texto) {
-  let mensaje = document.getElementById("mensaje");
+   let mensaje = document.getElementById("mensaje");
   mensaje.textContent = texto;
+  mensaje.style.opacity = 1;
 
   setTimeout(() => {
-    mensaje.textContent = "";
+    mensaje.style.opacity = 0;
   }, 2000);
+}
+
+function filtrarCategoria(cat){
+  let filtrar=inventario.filter(p=>p.categoria===cat);
+  lista.innerHTML="";
+  filtrar.forEach(producto=>{
+    let li=document.createElement("li");
+    li.textContent=`${producto.nombre} -$${producto.precio} (${producto.categoria})`
+    lista.appendChild(li);
+  });
+}
+
+function mostrarFormularioEditar(index){
+  
+   // cerrar todos primero
+  document.querySelectorAll('[id^="editar-"]').forEach(el => {
+    el.style.display = "none";
+  });
+  let form=document.getElementById(`editar-${index}`);//funcion para llamr el form de editar
+  form.style.display="block"
+;
+
+}
+  //boton de cancelar en el formulario de editar
+function cancelarGuardarDatos(index){
+ 
+  let form=document.getElementById(`editar-${index}`);
+  form.style.display="none";
+
 }
  mostrarProductos();
